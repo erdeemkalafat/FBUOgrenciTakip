a1b0614452a2b2853abc19c0dd2bdb24e87b9ae9
90537 <90537@ERDEM>
2022-03-07 21:03:22 +0300

    Add project files.

---
+++ DataAccessLayer/Entities/Ogrenci.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Ogrenci:BaseEntity
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Tel { get; set; }
+
+        public ICollection<Not> Nots { get; set; }
+    }
+}
---
+++ DataAccessLayer/Repositories/OgrRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class OgrRepository:BaseRepository<Ogrenci>
+    {
+        public OgrRepository(OgrDbContext ctx):base(ctx)
+        {
+           
+        }
+
+        public override Ogrenci GetById(int id)
+        {
+           return _ctx.Ogrenciler.Include(c=>c.Nots).FirstOrDefault(c => c.Id==id);
+        }
+
+        public List<Ogrenci> Search(string aranacakAd="", string AranacakSoyad="")
+        {
+            var query = _ctx.Ogrenciler.Where(c=>1==1);
+            
+            if (!String.IsNullOrEmpty(aranacakAd))
+            {
+                query = query.Where(c => c.Ad == aranacakAd);
+            }
+            if(!String.IsNullOrEmpty(AranacakSoyad))
+            {
+                query = query.Where(c => c.Soyad == AranacakSoyad);
+            }
+            return query.ToList();
+        }
+
+        
+
+        public List<myConfig> listCfg()
+        {
+            return _ctx.MyConfigs.ToList();
+        }
+
+    }
+}
